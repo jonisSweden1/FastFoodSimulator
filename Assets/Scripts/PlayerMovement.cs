ba3d9f640fc770b91c10f3470e2c0bf162110e5a
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2026-07-18 16:53:50 +0200

    Basic Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,20 +8,50 @@ public class PlayerMovement : MonoBehaviour
 
     private PlayerInput playerInput;
 
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 30f;
 
     private InputAction _moveAction;
     private InputAction _jumpAction;
 
     private void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
         controller = GetComponent<CharacterController>();
     }
 
     private void Start()
     {
-        InputSystem.actions.Disable();
+        playerInput = GetComponent<PlayerInput>();
         playerInput.currentActionMap?.Enable();
+    }
+
+    private void OnEnable()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.currentActionMap?.Enable();
+
+        _moveAction = playerInput.actions.FindAction("Move");
+        _jumpAction = playerInput.actions.FindAction("Jump");
+
+        _jumpAction.started += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        _jumpAction.started -= OnJump;
+
+        playerInput.currentActionMap?.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        // Handle continuous movement input
+        Vector2 moveInput = _moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        controller.Move(move * speed * Time.fixedDeltaTime);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // Implement jump logic here
     }
 }
